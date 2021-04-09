8390814e5e5a01120b4858c62c067abc30daa5d6
Yen <tou19880321@gmail.com>
2021-04-09 21:07:28 +0800

    DD

--- api.py
+++ api.py
@@ -1,13 +1,26 @@
-from fastapi import FastAPI
 from twstock import realtime
-print("ddddd")
+from fastapi import FastAPI, Query
+from typing import List
 app = FastAPI()
+
+def _format_stock_info(data) -> dict:
+    result = []
+    if 'success' in data: del data['success']
+    for x in data:
+        result.append({
+            'Code': data[x]['info']['code'],
+            'Name': data[x]['info']['name'],
+            'Open': data[x]['realtime']['open'],
+            'High': data[x]['realtime']['high'],
+            'Low': data[x]['realtime']['low'],
+            'Last': data[x]['realtime']['latest_trade_price'],
+            'SendTime': data[x]['info']['time'],
+        })
+
+    return result
+
+
 @app.get('/stock')
 
-
-
-
-
-def stock():
-    """Test endpoint"""
-    return realtime.get(['2330', '2836', '2834', '2832', '2823', '2838', '2887', '6005', '5880'])
+def stock(id_list: List[str] = Query(None)):
+    return _format_stock_info(realtime.get(id_list))
